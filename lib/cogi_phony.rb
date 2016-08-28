2c655f8cc6d24c6720b68a1f60fa5a5a03558fc2
Hoang Hoa <nobi.hoa@gmail.com>
2016-08-28 10:26:39 +0700

    Add country codes yaml

--- lib/cogi_phony.rb
+++ lib/cogi_phony.rb
@@ -1,7 +1,12 @@
 require 'cogi_phony/version'
+require 'yaml'
 
 module CogiPhony
   def self.hi
     puts "Hello from Cogi::Phony"
+  end
+
+  def self.country_codes_hash
+    @country_codes_hash ||= YAML.load_file(File.join(File.dirname(File.expand_path(__FILE__)), 'data/country_codes.yaml'))
   end
 end
--- spec/cogi_phony_spec.rb
+++ spec/cogi_phony_spec.rb
@@ -8,4 +8,28 @@ describe CogiPhony do
       expect(CogiPhony.hi).to eq(nil)
     end
   end
+
+  describe '#country_codes_hash' do
+    let(:country_codes) { CogiPhony.country_codes_hash }
+
+    it 'return a hash' do
+      expect(country_codes).to be_a(Hash)
+    end
+
+    it 'has countries key' do
+      expect(country_codes['countries']).not_to be_nil
+    end
+
+    it 'return country code 84' do
+      expect(country_codes['countries']['VN']['country_code']).to eq('84')
+    end
+
+    it 'return country name Vietnam' do
+      expect(country_codes['countries']['VN']['name']).to eq('Viet Nam')
+    end
+
+    it 'return nil if invalid iso code' do
+      expect(country_codes['countries']['ZZ']).to be_nil
+    end
+  end
 end
